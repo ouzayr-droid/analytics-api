926d0f6e4fefd2ec1e73f4ca3b17b36d913a12b3
ouzayr-droid <salifououzerou25@gmail.com>
2025-11-15 17:23:41 +0100

    création de middleware post et ajout des routes correspondantes

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter
-from .schemas import EventSchemas
+from .schemas import EventSchemas, EventListSchemas, EventCreateSchemas, EventUpdateSchemas
 
 router = APIRouter()
 
-# /api/router
+# /api/events
 @router.get("")
-def read_events():
-    return {"items": [1,2,3]}
+def read_events() -> EventListSchemas:
+    return {
+        "results": [{id:1},{id:2},{id:3}], 
+        "count":3
+    }
 
+# GET /api/events/number
 @router.get("/{event_id}")
 # -> EventSchemas, indique que la fonction est censé faire un retour d'un objet de type EventSchemas
 def get_event(event_id:int) -> EventSchemas:
     return {"id": event_id}
+
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,5 +1,17 @@
 # design la manière dont les données seront présentées dans la logique métier
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class EventCreateSchemas(BaseModel):
+    page: str
+    description: Optional[str] = Field(default="")
+    
+class EventUpdateSchemas(BaseModel):
+    description: str
 
 class EventSchemas(BaseModel):
-    id: int 
+    id: int
+
+class EventListSchemas:
+    results: List[EventSchemas]
+    count: int
