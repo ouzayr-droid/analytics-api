aaa07fa5322eb4b2b9a085ce8d9699ffb56f3553
ouzayr-droid <salifououzerou25@gmail.com>
2025-11-18 00:16:46 +0100

    requêtes sql 2

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,5 +1,5 @@
 # import os
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from .models import EventModel, EventListSchemas, EventCreateSchemas, EventUpdateSchemas
 from api.db.session import get_session
@@ -9,11 +9,12 @@ from api.db.config import DATABASE_URL
 
 # /api/events
 @router.get("/", response_model=EventListSchemas)
-def read_events():
-    query = select()
+def read_events(session: Session = Depends(get_session)):
+    query = select(EventModel).order_by(EventModel.id.desc()) #.limit(10)
+    results = session.exec(query).all()
     return {
-        "results": [{"id":1},{"id":2},{"id":3}], 
-        "count":3
+        "results": results, 
+        "count": len(results)
     }
 
 # POST /api/events
@@ -28,10 +29,14 @@ def create_event(payload:EventCreateSchemas, session: Session = Depends(get_sess
     return obj
 
 # GET /api/events/number
-@router.get("/{event_id}")
+@router.get("/{event_id}", response_model=EventModel)
 # -> EventSchemas, indique que la fonction est censé faire un retour d'un objet de type EventSchemas
-def get_event(event_id:int) -> EventModel:
-    return {"id": event_id, "page": "str", "description": "x"}
+def get_event(event_id:int, session: Session = Depends(get_session)):
+    query = select(EventModel).where(EventModel.id == event_id)
+    result = session.exec(query).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Event not found !")
+    return result
 
 # @router@get(event_id:int) -> EventModel:
     # a single
